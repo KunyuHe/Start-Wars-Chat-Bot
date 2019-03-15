0510b7762152e90313665956c6051b89eab3d161
Kunyu He <kunyuhe@uchicago.edu>
2019-03-14 23:40:00 -0500

    Got scripts ready

--- etl_get_scripts_bs4.py
+++ etl_get_scripts_bs4.py
@@ -11,9 +11,14 @@ Author:      Kunyu He
 import requests
 import bs4
 
+from functools import reduce
+
+REPLACE = [("\xa0", " "), ("\t", "    "), ("\xe9", "e"), ('\xc9', "E"),
+           ("’", "'"), ("‘", "'")]
+
 
 #----------------------------------------------------------------------------#
-def get_script_from_url(url, name, lst_to_replace, encoding=None):
+def get_script_from_url(url, name, lst_to_replace):
     """
     Takes a url and parse Star Wars script from the webpage (cerdit to:
     https://www.imsdb.com/scripts/) with BeautifulSoup. Write the parsed
@@ -24,7 +29,6 @@ def get_script_from_url(url, name, lst_to_replace, encoding=None):
         - name (string): output file name
         - lst_to_replace (list of strings): [string to be replaced,
                                              string for replacement]
-        - encoding (string): encoding method
 
     Returns:
         (None)
@@ -33,18 +37,14 @@ def get_script_from_url(url, name, lst_to_replace, encoding=None):
     soup = bs4.BeautifulSoup(page, "html5lib")
     script = soup.find('td', class_="scrtext").text
 
-    if not encoding:
-        with open('[Star-Wars-Chat-Bot]data/Scripts/' + name, 'w') as file:
-            file.write(script.replace(*lst_to_replace))
-    else:
-        with open('[Star-Wars-Chat-Bot]data/Scripts/' + name, 'wb') as file:
-            file.write(script.replace(*lst_to_replace).encode(encoding))
+    with open('[Star-Wars-Chat-Bot]data/Scripts/' + name, 'w') as file:
+        file.write(reduce(lambda string, rep: string.replace(*rep),
+                          lst_to_replace, script))
 
 
 #----------------------------------------------------------------------------#
 URL = "https://www.imsdb.com/scripts/Star-Wars-Revenge-of-the-Sith.html"
-get_script_from_url(URL, "EpisodeIII_script.txt", ["\xa0", " "])
+get_script_from_url(URL, "EpisodeIII_script.txt", REPLACE)
 
 URL = "https://www.imsdb.com/scripts/Star-Wars-Attack-of-the-Clones.html"
-get_script_from_url(URL, "EpisodeII_script.txt", ["\t", "    "],
-                    encoding='utf8')
+get_script_from_url(URL, "EpisodeII_script.txt", REPLACE)
