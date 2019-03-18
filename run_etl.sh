fa34e7972545115aec17990623d9b0c8776e30d5
Kunyu He <kunyuhe@uchicago.edu>
2019-03-18 15:22:15 -0500

    Upload Files

--- run_etl.sh
+++ run_etl.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+pip install --user -r requirements.txt
+sudo apt-get install python-tk3 uccs
+
 python etl_get_scripts_bs4.py
 python etl_get_dialogues.py
 python etl_clean_dialogues.py
 python etl_get_character_dialogues.py
+python etl_get_training_data.py
